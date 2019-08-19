e111a7dfd3afce30092ca4f4426865427336c561
Zwero <Zwero@zw.com>
2019-08-19 23:39:47 +0800

    搜索跳转搜索列表页面ok--提交

--- pages/search/index.js
+++ pages/search/index.js
@@ -6,90 +6,73 @@ Page({
    * 页面的初始数据
    */
   data: {
-    current: 0,
-    pagenum: 1,
-    pagesize: 10,
-    keyword: "",
-    goods: [],
-    // 是否有更多
-    hasMore: true,
-    inputValue: ""
+    inputValue: "", // 输入框的值
+    history: [] // 本地的历史记录
   },
   /**
   * 生命周期函数--监听页面加载
   */
   onLoad: function (options) {
+
+  },
+
+// 清空输入框
+  handleCancel() {
     this.setData({
-      keyword: options.keyword
+      inputValue: ""
     })
-    this.getData()
   },
 
-  // 点击取消搜索事件
-  cancelSearch() {
-
-  },
-
-  // tab 栏样式变化
-  handleChange(event) {
-    console.log('tab栏变化')
+  // m每次输入关键字的时候都会触发
+  handleInput(event) {
+    console.log(123)
     console.log(event)
-    const { index } = event.currentTarget.dataset;
+    // value是输入框的值
+    const { value } = event.detail;
+
+    // 把输入框的值保存到data
     this.setData({
-      current: index
-    })
-  },
-
-  // 获取数据
-  getData() {
-    // console.log(123)
-    const { pagenum, pagesize, keyword } = this.data
-    request({
-      url: "/goods/search",
-      data: {
-        query: keyword,
-        pagenum,
-        pagesize
-      }
-    }).then(res => {
-      const { goods } = res.data.message
-      // 是否满足pagesize条数。不满足说明是最后一页
-      if (goods.length < this.data.pagesize) {
-        this.setData({
-          hasMore: false
-        })
-      }
-
-      // 循环给每个商品修改价格，保留两位小数点
-      const newGoods = goods.map(v => {
-        v.goods_price = Number(v.goods_price).toFixed(2);
-        return v;
-      })
-
-      this.setData({
-        // 合并新旧的数据
-        goods: [...this.data.goods, ...newGoods]
-      })
-    })
-
-
-  },
-
-  // 下拉
-  onReachBottom() {
-    // 没有更多, 直接放回
-    // console.log(567)
-    // console.log(this.data.hasMore)
-
-    if (!this.data.hasMore) {
-      return;
-    }
-    // 页数加1
-    // console.log(321)
-    // 页数加1
-    this.setData({
-      pagenum: this.data.pagenum + 1
+      inputValue: value
     });
-    this.getData()
+
+    // 查询搜索建议
+    // request({
+    //   url:"/goods/qsearch",
+    //   data: {
+    //     query: value
+    //   }
+    // }).then(res => {
+    //   console.log(res)
+    // })
+  },
+
+  // 输入框点击确定(enter) 按钮时触发, 手机右下角的键
+  handleConfirm() {
+    console.log(456)
+     // 跳转到搜索列表页
+    // 类似vue中this.$router.push()
+    // 文档地址：https://developers.weixin.qq.com/miniprogram/dev/api/route/wx.navigateTo.html
+    wx.navigateTo({
+      url: "/pages/search_list/index?keyword=" + this.data.inputValue
+    })
+
+    // 保存新的搜索关键字前, 要把关键字添加已有的列表
+    const arr = [this.data.inputValue, ...this.data.history];
+    this.setData({
+      history: [...new Set(arr)]
+    })
+
+    // 把关键字保存到本地
+    // 文档地址：https://developers.weixin.qq.com/miniprogram/dev/api/storage/wx.setStorageSync.html
+    wx.setStorageSync("history", this.data.history)
+  },
+
+  // 清除所有的记录
+  handleClearAll() {
+   // 文档地址：https://developers.weixin.qq.com/miniprogram/dev/api/storage/wx.removeStorage.html
+    wx.removeStorageSync('history');
+    this.setData({
+      history:[]
+    })
   }
 })
