64175101ade030fdcb1a3fe18c106146f950a863
Zwero <Zwero@zw.com>
2019-08-20 19:44:12 +0800

    添加商品数据并保存到本地并且跳转到购物车页面(并渲染)一部分功能ok--提交

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -5,62 +5,80 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    address: {
+      userName: "", // 收货人
+      telNumber: "", // 电话
+      detaliInfo: "" // 地址
+    },
+    goods: {}
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+  // 当切换页面的时候重新加载数据
+  onShow: function(options) {
+    // 获取本地存储的数据
+    const goods = wx.getStorageSync("goods") || {}
 
+    this.setData({
+      goods
+    })
+    console.log(this.data.goods, '这是保存到对象里的')
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
+  // 收货地址
+  handleGetAddress() {
 
+    wx.chooseAddress({
+      success: res => {
+        console.log(res, "收货地址")
+        console.log(123)
+        // 获取收货地址
+        this.setData({
+          address: {
+            userName: res.userName,
+            telNumber: res.telNumber,
+            detaliInfo: res.provinceName + res.cityName + res.countyName + res.detailInfo
+          }
+        })
+        console.log(this.data.address)
+      }
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
+  // 计算总价格
+  getAllprice(){
+    let price = 0;
 
+    Object.keys(this.data.goods).forEach(v => {
+      price += this.data.goods[v].goods-price;
+    })
+
+    this.setData({
+      allPrice: price
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
+  // 选中状态的方法
+  handleSelected(e){
+    console.log(e)
+    const { id } = e.currentTarget.dataset
 
-  },
+    const {goods} = this.data
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
+    // 把选中状态取反
+    goods[id].selected = !goods[id].selected
 
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+    this.setData({
+      goods
+    })
+    // 计算总价
+    this.getAllprice()
   }
+
 })
--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -23,7 +23,7 @@ Page({
     request({
       url: "/goods/detail",
       data: {
-        goods_id: id
+        goods_id: 3
       }
     }).then(res => {
       // console.log(res,'商品详情数据')
@@ -36,4 +36,29 @@ Page({
     })
   },
 
+  // 点击购物车保存数据到本地
+  handleCart(e){
+    // console.log("触发")
+    console.log(e)
+
+    wx.showToast({
+      title: '添加购物车成功',
+      icon: 'success',
+      duration: 2000
+    });
+
+    const goods = wx.getStorageSync('goods') || {};
+    // 把商品添加到本地的goods对象中
+    // var goods ={}
+    const {detail} = this.data;
+
+    // 添加默认的选中状态和默认的数量
+    detail.selected = true;
+    detail.number = 1;
+
+    goods[detail.goods_id] = detail;
+    // 保存到本地
+    wx.setStorageSync("goods", goods)
+  }
+  
 })
